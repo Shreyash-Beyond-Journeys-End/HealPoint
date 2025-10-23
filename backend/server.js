3fc1c83df1310f50a7e8c4e5ed95142b3086f5cf
Shreyash kawde <shreyashkawde0@gmail.com>
2025-10-23 18:55:06 +0530

    Implement authentication endpoints (register, login, logout)

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,28 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const config = require("./config");
 const connectToDatabase = require("./db/mongoose");
+const corsMiddleware = require("./middlewares/cors");
+const errorHandlerMiddleware = require("./middlewares/errorHandler");
+const authController = require("./controllers/authController");
+const limiter = require("./middlewares/rateLimiter");
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(corsMiddleware);
 
 // Basic route to test server
 app.get("/", (req, res) => {
   res.json({ message: "HealPoint Backend Server Running" });
 });
+
+// Auth routes
+app.use("/auth", limiter, authController);
+
+// Error handler middleware
+app.use(errorHandlerMiddleware);
 
 // Start Server
 (async () => {
