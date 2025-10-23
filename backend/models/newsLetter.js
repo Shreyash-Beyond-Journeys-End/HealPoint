301a39ec05648cc00c6ac21fd9e43a8e3f66c8cb
Shreyash kawde <shreyashkawde0@gmail.com>
2025-10-23 18:51:09 +0530

    Add remaining models (Nurse, Department, ContactUs, NewsLetter, Communication)

---
+++ backend/models/newsLetter.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const newsletterSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+});
+
+const NewsLetter = mongoose.model("NewsLetter", newsletterSchema, "newsletter");
+module.exports = NewsLetter;
